6e7b2b560472eada9d7bf579582741e9e23ad38f
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-12-07 11:39:24 +1000

    fixing create button

--- src/api/post-project.js
+++ src/api/post-project.js
@@ -1,26 +1,26 @@
 async function postProject(title, description, goal, image) {
     const url = `${import.meta.env.VITE_API_URL}/projects/`;
+    const token = window.localStorage.getItem("token");
+
     const response = await fetch(url, {
-        method: "POST", // We need to tell the server that we are sending JSON data so we set the Content-Type header to application/json
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
+            "Authorization": `Token ${token}`,
         },
         body: JSON.stringify({
-            "title": title,
-            "description": description,
-            "goal": goal,
-            "image": image,
-            "is_open": true,
+            title: title,
+            description: description,
+            goal: goal,
+            image: image,
         }),
     });
 
     if (!response.ok) {
         const fallbackError = `Error trying to create project`;
-
         const data = await response.json().catch(() => {
             throw new Error(fallbackError);
         });
-
         const errorMessage = data?.detail ?? fallbackError;
         throw new Error(errorMessage);
     }
